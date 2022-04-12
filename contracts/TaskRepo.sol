1bb7e12e4cc43ff078e2b59466f5a46f4d6a4ea7
vsevolod-medvedev <vsevolod.medvedev@gmail.com>
2022-04-12 22:25:47 +0700

    Update completed percent calculation

--- contracts/TaskRepo.sol
+++ contracts/TaskRepo.sol
@@ -38,6 +38,7 @@ contract TaskRepo {
         uint createdTime = block.timestamp;
         Task memory task = Task(id, msg.sender, createdTime, _estimatedTimeInSeconds, Status.Pending, false, false);
         tasks.push(task);
+        _updateUserToTasksCompletedInTimePercent(msg.sender);
         emit TaskCreated(task.id, task.owner, task.createdTime, task.estimatedTimeInSeconds);
     }
 
@@ -65,6 +66,7 @@ contract TaskRepo {
 
     function deleteTask(uint _id) public ownerOnly(_id) notDeleted(_id) {
         tasks[_id].isDeleted = true;
+        _updateUserToTasksCompletedInTimePercent(msg.sender);
         emit TaskDeleted(_id);
     }
 
@@ -76,28 +78,30 @@ contract TaskRepo {
         task.status = _status;
         if (_status == Status.Completed) {
             task.isCompletedInTime = block.timestamp <= task.createdTime + task.estimatedTimeInSeconds;
-            emit TaskCompleted(_id, task.isCompletedInTime);
             _updateUserToTasksCompletedInTimePercent(msg.sender);
-        }        
+            emit TaskCompleted(_id, task.isCompletedInTime);
+        }
     }
 
     function _updateUserToTasksCompletedInTimePercent(address _user) private {
         uint inTimeCount = 0;
-        uint totalCount = 0;
+        uint completedCount = 0;
         for (uint i = 0; i < tasks.length; i++) {
             Task memory task = tasks[i];
             if (!task.isDeleted && task.owner == _user) {
-                totalCount++;
+                if (task.status == Status.Completed) {
+                    completedCount++;
+                }
                 if (task.isCompletedInTime) {
                     inTimeCount++;
                 }
             }
         }
         uint percent;
-        if (totalCount == 0) {
+        if (completedCount == 0) {
             percent = 0;
         } else {
-            percent = inTimeCount * 100 / totalCount;
+            percent = inTimeCount * 100 / completedCount;
         }
         userToTasksCompletedInTimePercent[_user] = percent;
     }
